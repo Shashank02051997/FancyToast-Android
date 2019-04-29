4bbcff5badd77e23c77cfae78f127a02f43cb3f5
shashank singhal <shashank.singhal@minemarksolutions.com>
2019-04-29 21:17:10 +0530

    Modify gradle version and new version release

--- app/src/main/java/com/shashank/sony/fancytoastlibrary/MainActivity.java
+++ app/src/main/java/com/shashank/sony/fancytoastlibrary/MainActivity.java
@@ -9,19 +9,20 @@ import android.widget.Toast;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button b1,b2,b3,b4,b5,b6,b7,b8;
+    Button b1, b2, b3, b4, b5, b6, b7, b8;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1=(Button) findViewById(R.id.button);
-        b2=(Button) findViewById(R.id.button2);
-        b3=(Button) findViewById(R.id.button3);
-        b4=(Button) findViewById(R.id.button4);
-        b5=(Button) findViewById(R.id.button5);
-        b6=(Button) findViewById(R.id.button6);
-        b7=(Button) findViewById(R.id.button7);
-        b8=(Button) findViewById(R.id.button8);
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
+        b5 = (Button) findViewById(R.id.button5);
+        b6 = (Button) findViewById(R.id.button6);
+        b7 = (Button) findViewById(R.id.button7);
+        b8 = (Button) findViewById(R.id.button8);
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
@@ -31,22 +32,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         b7.setOnClickListener(this);
         b8.setOnClickListener(this);
     }
+
     @Override
-    public void onClick(View v)
-    {   if(v.getId()==R.id.button)
-        FancyToast.makeText(this,"This is Default Toast",FancyToast.LENGTH_LONG,FancyToast.DEFAULT,true).show();
-        else if(v.getId()==R.id.button2)
-        FancyToast.makeText(this,"Success Toast !",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
-        else if(v.getId()==R.id.button3)
-        FancyToast.makeText(this,"This is an Error Toast",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
-        else if(v.getId()==R.id.button4)
-        FancyToast.makeText(this,"Beware of dog",FancyToast.LENGTH_LONG,FancyToast.WARNING,true).show();
-        else if(v.getId()==R.id.button5)
-        FancyToast.makeText(this,"Here is some Info for you",FancyToast.LENGTH_LONG,FancyToast.INFO,true).show();
-        else if(v.getId()==R.id.button6)
-        FancyToast.makeText(this,"This is Confusing Toast",FancyToast.LENGTH_LONG,FancyToast.CONFUSING,false).show();
-        else if(v.getId()==R.id.button7)
-        FancyToast.makeText(this,"This is Custom Toast",FancyToast.LENGTH_LONG,FancyToast.CONFUSING,R.drawable.ic_android_black_24dp).show();
-        else if(v.getId()==R.id.button8)
-        FancyToast.makeText(this,"This is a Success Toast",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();    }
+    public void onClick(View v) {
+        if (v.getId() == R.id.button)
+            FancyToast.makeText(this, "This is Default Toast", FancyToast.LENGTH_LONG, FancyToast.DEFAULT, true).show();
+        else if (v.getId() == R.id.button2)
+            FancyToast.makeText(this, "Success Toast !", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+        else if (v.getId() == R.id.button3)
+            FancyToast.makeText(this, "This is an Error Toast", FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+        else if (v.getId() == R.id.button4)
+            FancyToast.makeText(this, "Beware of dog", FancyToast.LENGTH_LONG, FancyToast.WARNING, true).show();
+        else if (v.getId() == R.id.button5)
+            FancyToast.makeText(this, "Here is some Info for you", FancyToast.LENGTH_LONG, FancyToast.INFO, true).show();
+        else if (v.getId() == R.id.button6)
+            FancyToast.makeText(this, "This is Confusing Toast", FancyToast.LENGTH_LONG, FancyToast.CONFUSING, false).show();
+        else if (v.getId() == R.id.button7)
+            FancyToast.makeText(this, "This is Custom Icon Toast", FancyToast.LENGTH_LONG, FancyToast.CONFUSING, R.drawable.ic_android_black_24dp).show();
+        else if (v.getId() == R.id.button8)
+            FancyToast.makeText(this, "This is a Success Toast", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
+    }
 }
--- fancytoastlib/src/main/java/com/shashank/sony/fancytoastlib/FancyToast.java
+++ fancytoastlib/src/main/java/com/shashank/sony/fancytoastlib/FancyToast.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,87 +19,80 @@ public class FancyToast extends Toast {
      * can call {@link #show}.
      *
      * @param context The context to use.  Usually your {@link Application}
-     *                or {@link Activity} object.
+     * or {@link Activity} object.
      */
-    public static int SUCCESS=1;
-    public static int WARNING=2;
-    public static int ERROR=3;
-    public static int INFO=4;
-    public static int DEFAULT=5;
-    public static int CONFUSING=6;
+    public static int SUCCESS = 1;
+    public static int WARNING = 2;
+    public static int ERROR = 3;
+    public static int INFO = 4;
+    public static int DEFAULT = 5;
+    public static int CONFUSING = 6;
+
     public FancyToast(Context context) {
         super(context);
     }
-    public static Toast makeText(Context context,String message,int duration,int type,boolean androidicon){
+
+    public static Toast makeText(Context context, String message, int duration, int type, boolean androidicon) {
         Toast toast = new Toast(context);
         toast.setDuration(duration);
         View layout = LayoutInflater.from(context).inflate(R.layout.fancytoast_layout, null, false);
         TextView l1 = (TextView) layout.findViewById(R.id.toast_text);
-        LinearLayout linearLayout=(LinearLayout) layout.findViewById(R.id.toast_type);
-        ImageView img=(ImageView) layout.findViewById(R.id.toast_icon);
-        ImageView img1=(ImageView) layout.findViewById(R.id.imageView4);
+        LinearLayout linearLayout = (LinearLayout) layout.findViewById(R.id.toast_type);
+        ImageView img = (ImageView) layout.findViewById(R.id.toast_icon);
+        ImageView img1 = (ImageView) layout.findViewById(R.id.imageView4);
         l1.setText(message);
-        if(androidicon==true)
-        img1.setVisibility(View.VISIBLE);
-        else if(androidicon==false)
-        img1.setVisibility(View.GONE);
-        if(type==1)
-        { linearLayout.setBackgroundResource(R.drawable.success_shape);
-          img.setImageResource(R.drawable.ic_check_black_24dp);
-        }
-        else if (type==2)
-        { linearLayout.setBackgroundResource(R.drawable.warning_shape);
-          img.setImageResource(R.drawable.ic_pan_tool_black_24dp);
-        }
-        else if (type==3)
-        { linearLayout.setBackgroundResource(R.drawable.error_shape);
-          img.setImageResource(R.drawable.ic_clear_black_24dp);
-        }
-        else if (type==4)
-        { linearLayout.setBackgroundResource(R.drawable.info_shape);
-          img.setImageResource(R.drawable.ic_info_outline_black_24dp);
-        }
-        else if (type==5)
-        { linearLayout.setBackgroundResource(R.drawable.default_shape);
-          img.setVisibility(View.GONE);
-        }
-        else if (type==6)
-        { linearLayout.setBackgroundResource(R.drawable.confusing_shape);
-          img.setImageResource(R.drawable.ic_refresh_black_24dp);
+        if (androidicon == true)
+            img1.setVisibility(View.VISIBLE);
+        else if (androidicon == false)
+            img1.setVisibility(View.GONE);
+        if (type == 1) {
+            linearLayout.setBackgroundResource(R.drawable.success_shape);
+            img.setImageResource(R.drawable.ic_check_black_24dp);
+        } else if (type == 2) {
+            linearLayout.setBackgroundResource(R.drawable.warning_shape);
+            img.setImageResource(R.drawable.ic_pan_tool_black_24dp);
+        } else if (type == 3) {
+            linearLayout.setBackgroundResource(R.drawable.error_shape);
+            img.setImageResource(R.drawable.ic_clear_black_24dp);
+        } else if (type == 4) {
+            linearLayout.setBackgroundResource(R.drawable.info_shape);
+            img.setImageResource(R.drawable.ic_info_outline_black_24dp);
+        } else if (type == 5) {
+            linearLayout.setBackgroundResource(R.drawable.default_shape);
+            img.setVisibility(View.GONE);
+        } else if (type == 6) {
+            linearLayout.setBackgroundResource(R.drawable.confusing_shape);
+            img.setImageResource(R.drawable.ic_refresh_black_24dp);
         }
         toast.setView(layout);
         return toast;
     }
-    public static Toast makeText(Context context,String message,int duration,int type,int ImageResource){
+
+    public static Toast makeText(Context context, String message, int duration, int type, int ImageResource) {
         Toast toast = new Toast(context);
         View layout = LayoutInflater.from(context).inflate(R.layout.fancytoast_layout, null, false);
         TextView l1 = (TextView) layout.findViewById(R.id.toast_text);
-        LinearLayout linearLayout=(LinearLayout) layout.findViewById(R.id.toast_type);
-        ImageView img=(ImageView) layout.findViewById(R.id.toast_icon);
+        LinearLayout linearLayout = (LinearLayout) layout.findViewById(R.id.toast_type);
+        ImageView img = (ImageView) layout.findViewById(R.id.toast_icon);
         l1.setText(message);
-        if(type==1)
-        { linearLayout.setBackgroundResource(R.drawable.success_shape);
-          img.setImageResource(ImageResource);
-        }
-        else if (type==2)
-        { linearLayout.setBackgroundResource(R.drawable.warning_shape);
-          img.setImageResource(ImageResource);
-        }
-        else if (type==3)
-        { linearLayout.setBackgroundResource(R.drawable.error_shape);
-          img.setImageResource(ImageResource);
-        }
-        else if (type==4)
-        { linearLayout.setBackgroundResource(R.drawable.info_shape);
-          img.setImageResource(ImageResource);
-        }
-        else if (type==5)
-        { linearLayout.setBackgroundResource(R.drawable.default_shape);
-          img.setVisibility(View.GONE);
-        }
-        else if (type==6)
-        { linearLayout.setBackgroundResource(R.drawable.confusing_shape);
-          img.setImageResource(ImageResource);
+        if (type == 1) {
+            linearLayout.setBackgroundResource(R.drawable.success_shape);
+            img.setImageResource(ImageResource);
+        } else if (type == 2) {
+            linearLayout.setBackgroundResource(R.drawable.warning_shape);
+            img.setImageResource(ImageResource);
+        } else if (type == 3) {
+            linearLayout.setBackgroundResource(R.drawable.error_shape);
+            img.setImageResource(ImageResource);
+        } else if (type == 4) {
+            linearLayout.setBackgroundResource(R.drawable.info_shape);
+            img.setImageResource(ImageResource);
+        } else if (type == 5) {
+            linearLayout.setBackgroundResource(R.drawable.default_shape);
+            img.setVisibility(View.GONE);
+        } else if (type == 6) {
+            linearLayout.setBackgroundResource(R.drawable.confusing_shape);
+            img.setImageResource(ImageResource);
         }
         toast.setView(layout);
         return toast;
